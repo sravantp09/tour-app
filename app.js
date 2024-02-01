d8111635b45ee0767d0a8064352131aba48c2ff2
Sravan T P <sravantp99@gmail.com>
2024-02-01 11:22:28 +0530

    added api endpoint to create new tours

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const toursInfo = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'),
 );
 
+// middleware
+app.use(express.json());
+
 // get all tours information
 app.get('/api/v1/tours', (req, res) => {
   if (!toursInfo || toursInfo.length === 0)
@@ -23,6 +26,36 @@ app.get('/api/v1/tours', (req, res) => {
     results: toursInfo?.length,
     data: { tours: toursInfo },
   });
+});
+
+// creating new tour
+app.post('/api/v1/tours', (req, res) => {
+  const newTourId = toursInfo[toursInfo.length - 1].id + 1;
+  const newTour = {
+    id: newTourId,
+    ...req.body,
+  };
+  toursInfo.push(newTour);
+
+  // updating tours file
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(toursInfo),
+    (err) => {
+      if (err) {
+        return res.status(404).json({
+          status: 'error',
+          message: 'unable to create new tour',
+        });
+      }
+      return res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
 });
 
 app.listen(PORT, () => {
