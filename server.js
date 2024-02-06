eeb11e330c7b90bd0545a812d8c7e5ce493d31fb
Sravan T P <sravantp99@gmail.com>
2024-02-06 09:26:12 +0530

    database connection

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 require('dotenv').config({ path: '.env' });
 const app = require('./app.js');
+const mongoose = require('mongoose');
 
+const DB_CONNECTION = process.env.DB_CONNECTION;
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on Port : ${PORT}`);
-});
+// connection to the database
+mongoose
+  .connect(DB_CONNECTION)
+  .then((connection) => {
+    console.log('Database Connected...');
+
+    // starting server only after successful db connection
+    if (connection) {
+      app.listen(PORT, () => {
+        console.log(`Server is running on Port : ${PORT}`);
+      });
+    }
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
