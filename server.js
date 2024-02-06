b0730f6549a91df03b15fbd10730fb453da94ff1
Sravan T P <sravantp99@gmail.com>
2024-02-06 09:44:03 +0530

    create schema and model

--- server.js
+++ server.js
@@ -21,3 +21,23 @@ mongoose
   .catch((err) => {
     console.log(err.message);
   });
+
+// schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have price'],
+  },
+});
+
+// model
+const Tour = mongoose.model('Tour', tourSchema);
