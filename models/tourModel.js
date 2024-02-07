dde5d38ffea3b771ed04f9c85dfe698a80009f21
Sravan T P <sravantp99@gmail.com>
2024-02-07 09:18:52 +0530

    update schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,15 +6,54 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+    default: 1,
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficuty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true, // works only for string and removed white spaces from start and end
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  // name of the image
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have an image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 // tour model
