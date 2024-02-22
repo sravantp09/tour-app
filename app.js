d377c339d53602fc52c4f2844df7a7daaf6751e2
Sravan T P <sravantp99@gmail.com>
2024-02-22 09:13:35 +0530

    implement data sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit'); // RATE LIMITER
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const { xss } = require('express-xss-sanitizer');
 const tourRoute = require('./routes/toursRoute.js');
 const userRoute = require('./routes/userRoute.js');
 
@@ -33,6 +35,12 @@ app.use('/api', limiter);
 // parsing request body
 app.use(express.json({ limit: '10kb' })); // max size of body allowed is 10kb
 
+// DATA SANITIZATION (after body has been parsed)
+// 1. against NoSQL query injection
+app.use(mongoSanitize());
+
+// 2. against XSS (cross site scripting attack)
+app.use(xss());
 // making contents inside public directory available public
 app.use(express.static(`${__dirname}/public`));
 
