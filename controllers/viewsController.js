9b3bac40977fe0fa8fed19e6f93243ad94e23c01
Sravan T P <sravantp99@gmail.com>
2024-03-09 12:57:22 +0530

    implement overview + tour card pug template

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,19 @@
-exports.getOveriew = (req, res) => {
-  res.status(200).render('overview');
+const Tour = require('../models/tourModel.js');
+const AppError = require('../utils/appError.js');
+
+exports.getOveriew = async (req, res, next) => {
+  try {
+    // 1) get all tour data from the tour collection
+    const tours = await Tour.find();
+
+    // 2) build the tempate
+
+    // 3) render the template
+    return res.status(200).render('overview', { title: 'All Tours', tours });
+  } catch (err) {
+    console.log(err.message);
+    next(new AppError('Not Found', 404, err));
+  }
 };
 
 exports.getTour = (req, res) => {
--- routes/viewRoute.js
+++ routes/viewRoute.js
@@ -6,4 +6,9 @@ const { getOveriew, getTour } = require('../controllers/viewsController.js');
 router.get('/', getOveriew);
 router.get('/tour', getTour);
 
+// Adding redirection route for '/overview' to root router '/'
+router.get('/overview', (req, res) => {
+  return res.redirect('/');
+});
+
 module.exports = router;
